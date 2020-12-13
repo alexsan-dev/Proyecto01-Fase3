8962bb5c1c8ad726a120829205e02f8aa697329b
Alex Santos <alexdsantosv@gmail.com>
2020-12-13 00:51:57 -0600

    [ Add ] favicon

--- bank/urls.py
+++ bank/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('front.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("/images/favicon.ico")),
+    ),
     path('admin/', admin.site.urls),
 ]
