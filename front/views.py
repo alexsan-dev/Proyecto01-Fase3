3495dcee40ffc72c9c86ecf283aa0ba04c785855
Alex Santos <alexdsantosv@gmail.com>
2020-12-13 00:22:25 -0600

    [ Add ] signing and links

--- front/views.py
+++ front/views.py
@@ -3,6 +3,11 @@ from django.template.response import TemplateResponse
 # Create your views here.
 
 
-def index(request):
-    response = TemplateResponse(request, 'index.html', {})
+def login(request):
+    response = TemplateResponse(request, 'login.html', {})
+    return response
+
+
+def signing(request):
+    response = TemplateResponse(request, 'signing.html', {})
     return response
